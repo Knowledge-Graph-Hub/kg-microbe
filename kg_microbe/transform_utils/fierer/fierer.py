1e7dcfc26259af69d31e6b2d303bd7a4dbe06c2e
harshad <hrshdhgd@users.noreply.github.com>
2020-11-20 16:01:16 -0600

    added isolation source node and org-source edge

--- kg_microbe/transform_utils/fierer/fierer.py
+++ kg_microbe/transform_utils/fierer/fierer.py
@@ -30,9 +30,9 @@ class FiererDataTransform(Transform):
         source_name = "fierer"
         super().__init__(source_name, input_dir, output_dir)  # set some variables
 
-        self.node_header = ['id', 'entity', 'category', 'reference', 'ref_type']
+        self.node_header = ['id', 'entity', 'category', 'reference', 'CURIE']
         self.edge_header = ['subject', 'edge_label', 'object', 'relation',
-                            'reference', 'ref_type']
+                            'reference', 'CURIE']
 
     def run(self, data_file: Optional[str] = None):
         """Method is called and performs needed transformations to process the 
@@ -64,22 +64,22 @@ class FiererDataTransform(Transform):
             seen_organism: dict = defaultdict(int)
             seen_carbon_substrate: dict = defaultdict(int)
             seen_shape: dict = defaultdict(int)
-            seen_env: dict = defaultdict(int)
+            seen_isolation_source: dict = defaultdict(int)
             seen_metab_type: dict = defaultdict(int)
 
 
             # Nodes
-            org_node_type = "biolink:OrganismalEntity" # [org_name]
+            org_node_type = "biolink:OrganismTaxon" # [org_name]
             chem_node_type = "biolink:ChemicalSubstance" # [carbon_substrate]
             shape_node_type = "biolink:AbstractEntity" # [cell_shape]
-            activity_node_type = "biolink:ActivityAndBehavior" # [metabolism]
+            #metabolism_node_type = "biolink:ActivityAndBehavior" # [metabolism]
             source_node_type = "biolink:Association" # [isolation_source]
             
             #Prefixes
-            org_prefix = "Organism:"
+            org_prefix = "NCBITaxon:"
             chem_prefix = "Carbon:"
             shape_prefix = "Shape:"
-            activity_prefix = "Metab:"
+            #activity_prefix = "Metab:"
             source_prefix = "Env:"
 
             # Edges
@@ -87,7 +87,8 @@ class FiererDataTransform(Transform):
             org_to_shape_edge_relation = "RO:0002200" #  [org_name -> cell_shape, metabolism]
             org_to_chem_edge_label = "biolink:produces" # [org_name -> carbon_substrate]
             org_to_chem_edge_relation = "RO:0003000" # [org_name -> carbon_substrate]
-            org_tosource_edge_relation = ""# [org -> isolation_source]
+            org_to_source_edge_label = "biolink:EnvironmentalFeature"# [org -> isolation_source]
+            org_to_source_edge_relation = "ENVO:01000254"
 
             
             
@@ -117,7 +118,7 @@ class FiererDataTransform(Transform):
                 cell_shape = items_dict['cell_shape']
                 isolation_source = set([x.strip() for x in items_dict['isolation_source'].split('|')])
                 reference = items_dict['reference']
-                ref_type = items_dict['ref_type']
+                curie = "NEED-CURIE"
 
             # Write Node ['id', 'entity', 'category', 'reference', 'ref_type']
                 # Write organism node 
@@ -129,7 +130,7 @@ class FiererDataTransform(Transform):
                                                org_name,
                                                org_node_type,
                                                reference,
-                                               ref_type])
+                                               curie])
                     seen_organism[tax_id] += 1
 
                 # Write chemical node
@@ -142,7 +143,7 @@ class FiererDataTransform(Transform):
                                                 chem_name,
                                                 chem_node_type,
                                                 reference,
-                                                ref_type])
+                                                curie])
                         seen_carbon_substrate[chem_id] += 1
 
                 # Write shape node
@@ -154,8 +155,21 @@ class FiererDataTransform(Transform):
                                                cell_shape,
                                                shape_node_type,
                                                reference,
-                                               ref_type])
+                                               curie])
                     seen_shape[shape_id] += 1
+
+                # Write source node
+                for source_name in isolation_source:
+                    source_id = source_prefix + source_name.lower().replace(' ','_')
+                    if source_id not in seen_isolation_source:
+                        write_node_edge_item(fh=node,
+                                            header=self.node_header,
+                                            data=[source_id, # NEEDS TO BE UPDATED
+                                                source_name,
+                                                source_node_type,
+                                                reference,
+                                                curie])
+                        seen_isolation_source[source_id] += 1
 
                 
 
@@ -169,7 +183,7 @@ class FiererDataTransform(Transform):
                                                chem_id,
                                                org_to_chem_edge_relation,
                                                reference,
-                                               ref_type])
+                                               curie])
                 # org-shape edge
                 write_node_edge_item(fh=edge,
                                          header=self.edge_header,
@@ -178,11 +192,17 @@ class FiererDataTransform(Transform):
                                                shape_id,
                                                org_to_shape_edge_relation,
                                                reference,
-                                               ref_type])
-
-
-
-
+                                               curie])
+                
+                # org-source edge
+                write_node_edge_item(fh=edge,
+                                         header=self.edge_header,
+                                         data=[org_id,
+                                               org_to_source_edge_label,
+                                               source_id,
+                                               org_to_source_edge_relation,
+                                               reference,
+                                               curie])
                 #pdb.set_trace()
                 #pdb.set_trace = lambda:None
         return None

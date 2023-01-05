ca8d72cd1109c82488bc909f80117a33928e1b1c
caufieldjh <j.harry.caufield@gmail.com>
2023-01-05 11:51:07 -0500

    Linting and cleanup, pass the first

--- kg_microbe/transform.py
+++ kg_microbe/transform.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Top-level functions for transforming data."""
+
 import logging
 from typing import List
 
@@ -7,28 +7,26 @@ from kg_microbe.transform_utils.ontology import OntologyTransform
 from kg_microbe.transform_utils.ontology.ontology_transform import ONTOLOGIES
 from kg_microbe.transform_utils.traits.traits import TraitsTransform
 
-
 DATA_SOURCES = {
-    'TraitsTransform': TraitsTransform,
-    'NCBITransform': OntologyTransform,
-    'ChebiTransform': OntologyTransform,
-    'EnvoTransform' : OntologyTransform,
-    'GoTransform': OntologyTransform
+    "TraitsTransform": TraitsTransform,
+    "NCBITransform": OntologyTransform,
+    "ChebiTransform": OntologyTransform,
+    "EnvoTransform": OntologyTransform,
+    "GoTransform": OntologyTransform,
 }
 
 
 def transform(input_dir: str, output_dir: str, sources: List[str] = None) -> None:
-    """
-    Call scripts in kg_microbe/transform/[source name]/ to transform each source into a graph format that
-    KGX can ingest directly, in either TSV or JSON format:
-    https://github.com/NCATS-Tangerine/kgx/blob/master/data-preparation.md
+    """Call scripts in kg_microbe/transform/[source name]/ to transform data.
 
-    :param input_dir: A string pointing to the directory to import data from.
-    :param output_dir: A string pointing to the directory to output data to.
-    :param sources: A list of sources to transform.
-    :return: None.
+    KGX can ingest each directly, in either TSV or JSON format.
+    Args:
+        input_dir: A string pointing to the directory to import data from.
+        output_dir: A string pointing to the directory to output data to.
+        sources: A list of sources to transform.
+    Returns:
+        None.
     """
-    
     if not sources:
         # run all sources
         sources = list(DATA_SOURCES.keys())

60a52cfb53e1cc0f26f9135c5ea01960c289e5e1
caufieldjh <j.harry.caufield@gmail.com>
2023-01-05 11:18:45 -0500

    Replace downloader with kghub_downloader

--- kg_microbe/download.py
+++ kg_microbe/download.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Basic download function."""
 
-
-from .utils import download_from_yaml
+from kghub_downloader.download_utils import download_from_yaml
 
 
 def download(yaml_file: str, output_dir: str, ignore_cache: bool = False) -> None:
+    """Download data files from list of URLs into data directory.
+    Args:
+        yaml_file: A string pointing to the yaml file utilized
+        to facilitate the downloading of data.
+        output_dir: A string pointing to the location to download data to.
+        ignore_cache: Ignore cache and download files even if they exist [false]
+    Returns:
+        None.
     """
-    Downloads data files from list of URLs (default: download.yaml) into data directory (default: data/).
-
-    :param yaml_file: A string pointing to the yaml file utilized to facilitate the downloading of data.
-    :param output_dir: A string pointing to the location to download data to.
-    :param ignore_cache: Ignore cache and download files even if they exist [false]
-    :return: None.
-    """
-
-    download_from_yaml(yaml_file=yaml_file, output_dir=output_dir,
-                       ignore_cache=ignore_cache)
+    download_from_yaml(
+        yaml_file=yaml_file, output_dir=output_dir, ignore_cache=ignore_cache
+    )
 
     return None

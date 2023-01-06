09284934f4a57780e8132c2d3387da836eaf1443
caufieldjh <j.harry.caufield@gmail.com>
2023-01-06 11:05:03 -0500

    Download compressed CHEBI, convert to obojson

--- kg_microbe/transform_utils/ontology/ontology_transform.py
+++ kg_microbe/transform_utils/ontology/ontology_transform.py
@@ -1,22 +1,25 @@
 """Transform an ontology in Obograph JSON format."""
 
+import gzip
 import os
+import shutil
 from typing import Optional
 
 from kgx.cli.cli_utils import transform
 
 from kg_microbe.transform_utils.transform import Transform
+from kg_microbe.utils.robot_utils import convert_to_json
 
 ONTOLOGIES = {
     "NCBITransform": "ncbitaxon.json",
-    "ChebiTransform": "chebi.json",
+    "ChebiTransform": "chebi.owl.gz",
     "EnvoTransform": "envo.json",
     "GoTransform": "go.json",
 }
 
 
 class OntologyTransform(Transform):
-    """Parse an Obograph JSON form of an Ontology into nodes and edges."""
+    """Parse a raw form of an Ontology into nodes and edges."""
 
     def __init__(
         self, input_dir: Optional[str] = None, output_dir: Optional[str] = None
@@ -46,6 +49,10 @@ class OntologyTransform(Transform):
     def parse(self, name: str, data_file: str, source: str) -> None:
         """Process the data_file.
 
+        If the file is is compressed,
+        decompress it.
+        If the file isn't in obojson format,
+        convert it first.
         Args:
             name: Name of the ontology
             data_file: data file to parse
@@ -54,6 +61,25 @@ class OntologyTransform(Transform):
              None.
         """
         print(f"Parsing {data_file}")
+
+        convert_this = False
+        decompress_this = False
+
+        if name in ["chebi"]:
+            convert_this = True
+            decompress_this = True
+
+        if decompress_this:
+            print("Decompressing...")
+            outpath = data_file[:-3]
+            with gzip.open(data_file, "rb") as data_file_gz:
+                with open(outpath, "wb") as data_file_new:
+                    shutil.copyfileobj(data_file_gz, data_file_new)
+            
+        if convert_this:
+            print("Converting to obojson...")
+            convert_to_json(self.input_base_dir, "CHEBI")
+            data_file = os.path.join(self.input_base_dir, name + ".json")
 
         transform(
             inputs=[data_file],

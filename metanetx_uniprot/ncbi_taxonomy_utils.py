e3cf90a5251aef3950ddcdd63fc1a307040f3281
Brook Santangelo <70932395+bsantan@users.noreply.github.com>
2023-03-20 16:42:09 -0600

    Update ncbi_taxonomy_utils.py
    Update to use ncbi_taxon input from kg-microbe

--- metanetx_uniprot/ncbi_taxonomy_utils.py
+++ metanetx_uniprot/ncbi_taxonomy_utils.py
@@ -14,15 +14,23 @@ import tempfile
 import urllib
 from urllib.request import urlretrieve
 
+from kgx.cli.cli_utils import transform
+import pandas as pd
+
 
 __NCBITAXONOMY_URL = 'ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz'
 
 
 def load(writer, array_delimiter, source=__NCBITAXONOMY_URL):
     '''Loads NCBI Taxonomy data.'''
-    nodes_filename, names_filename = _get_ncbi_taxonomy_files(source)
-    nodes, rels = _parse_nodes(nodes_filename, array_delimiter)
-    _parse_names(nodes, names_filename, array_delimiter)
+    #nodes_filename, names_filename = _get_ncbi_taxonomy_files(source)
+    ####Update filepath accordingly
+    nodes_filename = '~/kg_microbe/kg-microbe/data/raw/ncbitaxon.json'
+    #nodes, rels = _parse_nodes(nodes_filename, array_delimiter)
+    print('parsing ncbi taxon json file')
+    kgx_nodes_json,kgx_edges_json = _parse_nodes_kgmicrobe(nodes_filename, array_delimiter)
+    nodes,rels = transform_kgx_output_format(kgx_nodes_json,kgx_edges_json)
+    #_parse_names(nodes, names_filename, array_delimiter)
 
     writer.write_nodes(nodes.values(), 'Organism')
     writer.write_rels(rels, 'Organism', 'Organism')
@@ -43,6 +51,40 @@ def _get_ncbi_taxonomy_files(source):
     return os.path.join(temp_dir, 'nodes.dmp'), \
         os.path.join(temp_dir, 'names.dmp')
 
+def _parse_nodes_kgmicrobe(filename, array_delimiter):
+    '''Parses nodes file.'''
+
+    ####Update filepath accordingly
+    output_dir = '~/biochem4j/'
+    name = 'ncbitaxon_transformed'
+    
+    transform(inputs=[filename], input_format='obojson', output= os.path.join(output_dir, name), output_format='tsv')
+
+    return output_dir+name+'_nodes.tsv',output_dir+name+'_edges.tsv'
+
+def transform_kgx_output_format(transformed_nodes_tsv,transformed_edges_tsv):
+
+    labels = pd.read_csv(transformed_nodes_tsv, sep = '\t', usecols = ['id','name'])
+    triples_df = pd.read_csv(transformed_edges_tsv,sep = '\t', usecols = ['subject', 'object', 'predicate'])
+    triples_df.columns.str.lower()
+
+    nodes = {}
+    rels = []
+
+    for i in range(len(labels)):
+        tax_id = labels.iloc[i].loc['id']
+        nodes[tax_id] = {'taxonomy:ID(Organism)': tax_id,
+                             ':LABEL':
+                             'Organism,unknown'}
+
+    for i in range(len(triples_df)):
+        s = triples_df.iloc[i].loc['subject']
+        p = triples_df.iloc[i].loc['predicate']
+        o = triples_df.iloc[i].loc['object']
+        rels.append([s, p, o])
+
+    return nodes,rels
+
 
 def _parse_nodes(filename, array_delimiter):
     '''Parses nodes file.'''
@@ -61,6 +103,7 @@ def _parse_nodes(filename, array_delimiter):
                              ':LABEL':
                              'Organism' + array_delimiter + tokens[2]}
 
+    print(list(nodes.values())[0:5])
     return nodes, rels
 
 

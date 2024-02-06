94be8c3cf857bc33621c2270592ac6d9b18f0ff7
Harshad Hegde <hegdehb@gmail.com>
2024-02-06 10:17:04 -0600

    download utils not needed any more and first stab at uniprot download

--- kg_microbe/transform_utils/uniprot/uniprot.py
+++ kg_microbe/transform_utils/uniprot/uniprot.py
@@ -1,3 +1,5 @@
+"""Uniprot Transonform class."""
+
 import csv
 import json
 import os
@@ -6,7 +8,6 @@ import sys
 from pathlib import Path
 from typing import Optional, Union
 
-from numpy import *
 from tqdm import tqdm
 
 from kg_microbe.transform_utils.constants import (
@@ -117,7 +118,7 @@ class UniprotTransform(Transform):
             if "Entry" in entry.keys():
                 self.__enz_data["id"] = entry["Entry"]
 
-            # example response with  mulitple protein names: {'Organism (ID)': '100', 'Entry Name': 'A0A4R1H4N5_ANCAQ', 'Entry': 'A0A4R1H4N5', 'Protein names': 'Ubiquinone biosynthesis O-methyltransferase (2-polyprenyl-6-hydroxyphenol methylase) (EC 2.1.1.222) (3-demethylubiquinone 3-O-methyltransferase) (EC 2.1.1.64)', 'EC number': '2.1.1.222; 2.1.1.64'}
+            # example response with  multiple protein names: {'Organism (ID)': '100', 'Entry Name': 'A0A4R1H4N5_ANCAQ', 'Entry': 'A0A4R1H4N5', 'Protein names': 'Ubiquinone biosynthesis O-methyltransferase (2-polyprenyl-6-hydroxyphenol methylase) (EC 2.1.1.222) (3-demethylubiquinone 3-O-methyltransferase) (EC 2.1.1.64)', 'EC number': '2.1.1.222; 2.1.1.64'}
             if "Protein names" in entry:
                 self.__enz_data["name"] = entry["Protein names"].split("(EC")[0]
 

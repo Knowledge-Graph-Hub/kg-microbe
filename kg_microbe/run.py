c83bb37ffbb5e4757946f4cc88255862cf5f66e5
bsantan <70932395+bsantan@users.noreply.github.com>
2024-03-26 22:56:18 -0600

    transform in progress

--- kg_microbe/run.py
+++ kg_microbe/run.py
@@ -9,7 +9,7 @@ from kg_microbe.merge_utils.merge_kg import load_and_merge
 from kg_microbe.query import parse_query_yaml, result_dict_to_tsv, run_query
 from kg_microbe.transform import DATA_SOURCES
 from kg_microbe.transform import transform as kg_transform
-from kg_microbe.utils.s3_utils import run_api
+#from kg_microbe.utils.s3_utils import run_api
 
 show_status_option = click.option("--show-status/--no-show-status", default=True)
 
@@ -117,7 +117,7 @@ def query(
     result_dict_to_tsv(result_dict, outfile)
 
 
-@main.command("get-via-api")
+'''@main.command("get-via-api")
 @click.option("api", "-a", required=True, default="uniprot", multiple=False)
 @show_status_option
 def get_data_via_api(api: str, show_status: bool) -> None:
@@ -127,7 +127,7 @@ def get_data_via_api(api: str, show_status: bool) -> None:
     :param api: A string pointing to the API to upload data to.
     :return: None
     """
-    run_api(api, show_status)
+    run_api(api, show_status)'''
 
 
 @main.command()

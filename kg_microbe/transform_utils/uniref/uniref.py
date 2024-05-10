888837777fd96b7e3fbd0b57e17271249fad7808
Harshad Hegde <hegdehb@gmail.com>
2024-05-10 13:35:58 -0500

    update progress by 500

--- kg_microbe/transform_utils/uniref/uniref.py
+++ kg_microbe/transform_utils/uniref/uniref.py
@@ -120,7 +120,7 @@ class UnirefTransform(Transform):
 
                     progress.set_description(f"Processing Cluster: {cluster_id}")
                     # After each iteration, call the update method to advance the progress bar.
-                    progress.update(100)
+                    progress.update(500)
                     gc.collect()
 
         drop_duplicates(self.output_node_file)

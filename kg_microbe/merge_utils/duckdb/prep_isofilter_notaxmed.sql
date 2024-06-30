50223e4e7bd1ed9f39c8a8107fe69864f2f1f441
realmarcin <realmarcin@users.noreply.github.com>
2024-06-30 11:35:14 -0700

    filter iso

--- kg_microbe/merge_utils/duckdb/prep_isofilter_notaxmed.sql
+++ kg_microbe/merge_utils/duckdb/prep_isofilter_notaxmed.sql
@@ -28,3 +28,20 @@ WHERE object LIKE 'medium:%' AND predicate = 'biolink:occurs_in';
 
 -- Step 3: Save the updated table to a new file
 COPY edges TO 'merged-kg_edges_filteriso_notaxmed.tsv' (HEADER, DELIMITER '\t');
+
+
+
+-- Step 1: Identify unique subjects and objects in the edges table
+CREATE TEMP TABLE subjects_and_objects AS
+SELECT DISTINCT subject AS node FROM edges
+UNION
+SELECT DISTINCT object AS node FROM edges;
+
+-- Step 2: Create a table for filtered nodes
+CREATE TEMP TABLE filtered_raw_nodes AS
+SELECT *
+FROM raw_nodes
+WHERE id IN (SELECT node FROM subjects_and_objects);
+
+-- Step 2: Save the updated raw_nodes table to a new file
+COPY filtered_raw_nodes TO 'merged-kg_nodes_filteriso.tsv' (HEADER, DELIMITER '\t');
